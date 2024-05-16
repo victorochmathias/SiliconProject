8c3cc2858a6896d60b7851c397b952956ada5efd
victor <myran_virre@hotmail.com>
2024-05-16 20:05:03 +0200

    sparar här..

--- BlazorWebApp/BlazorWebApp/Program.cs
+++ BlazorWebApp/BlazorWebApp/Program.cs
@@ -1,7 +1,7 @@
 using BlazorWebApp.Components;
 using BlazorWebApp.Components.Account;
 using BlazorWebApp.Data;
-
+using BlazorWebApp.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +19,9 @@ builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
 builder.Services.AddScoped<SignInManager<ApplicationUser>>();
+builder.Services.AddScoped<UserService>();
+
+
 
 
 builder.Services.AddAuthentication(options =>
