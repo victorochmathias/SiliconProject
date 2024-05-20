25e46c75a88f0d081300f30da00267d3161facc1
Mathias Rönnmark <mathiasronnmark@hotmail.com>
2024-05-20 13:14:17 +0200

    Added courses page

---
+++ BlazorWebApp.Business/Models/DynamicGraphQLResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BlazorWebApp.Business.Models;
+
+public class DynamicGraphQLResponse
+{
+    [JsonPropertyName("data")]
+    public JsonElement Data { get; set; }
+}
---
+++ BlazorWebApp.Business/Models/GraphQLQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorWebApp.Business.Models;
+
+public class GraphQLQuery
+{
+    public string Query { get; set; } = null!;
+}
